b647478ab767a545d0256b6ed7211cd160914555
DDigitale <alexdvox@gmail.com>
2023-03-26 20:33:20 +0300

    header whisper uncommented

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,13 +4,11 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import {
   fetchLogout,
   fetchPlayerSearch,
-  fetchServerInfo,
   fetchServerInfoWithPort,
 } from 'api/users'
 import { PlayerModalContext, PlayerModalContextType } from 'contexts'
 import useDebounce from 'components/debounce/useDebounce'
 import { fetchGetMe } from 'api/admins'
-
 import {
   Button,
   ButtonGroup,
@@ -65,17 +63,17 @@ function Header() {
     await window.location.replace('/')
   }
 
-  // const { data: serverOne } = useQuery(
-  //   ['serverOne'],
-  //   () => fetchServerInfoWithPort(8000),
-  //   { refetchInterval: 5000 }
-  // )
+  const { data: serverOne } = useQuery(
+    ['serverOne'],
+    () => fetchServerInfoWithPort(8000),
+    { refetchInterval: 5000 }
+  )
 
-  // const { data: serverTwo } = useQuery(
-  //   ['serverTwo'],
-  //   () => fetchServerInfoWithPort(8001),
-  //   { refetchInterval: 5000 }
-  // )
+  const { data: serverTwo } = useQuery(
+    ['serverTwo'],
+    () => fetchServerInfoWithPort(8001),
+    { refetchInterval: 5000 }
+  )
 
   useEffect(() => {
     if (debouncedSearch) {
@@ -99,35 +97,33 @@ function Header() {
     server = localStorage.getItem('server')
   }, [server])
 
-  // const speakerOne = (
-  //   <Popover title={serverOne?.serverName}>
-  //     <div>
-  //       <p>
-  //         Игроков: {serverOne?.playerCount}/100{' '}
-  //         {serverOne?.publicQueue === 0 ? `` : `(+ ${serverOne?.publicQueue})`}
-  //       </p>
-  //       <p>TPS: {serverOne?.serverTickRate}</p>
-  //       <p>Текущая карта: {serverOne?.currentLayer}</p>
-  //       <p>Следующая карта: {serverOne?.nextLayer}</p>
-  //     </div>
-  //   </Popover>
-  // )
+  const speakerOne = (
+    <Popover title={serverOne?.serverName}>
+      <div>
+        <p>
+          Игроков: {serverOne?.playerCount}/100{' '}
+          {serverOne?.publicQueue === 0 ? `` : `(+ ${serverOne?.publicQueue})`}
+        </p>
+        <p>TPS: {serverOne?.serverTickRate}</p>
+        <p>Текущая карта: {serverOne?.currentLayer}</p>
+        <p>Следующая карта: {serverOne?.nextLayer}</p>
+      </div>
+    </Popover>
+  )
 
-  console.log('Header.tsx включить спикер на второй сервер')
-
-  // const speakerTwo = (
-  //   <Popover title={serverTwo?.serverName}>
-  //     <div>
-  //       <p>
-  //         Игроков: {serverTwo?.playerCount}/100{' '}
-  //         {serverTwo?.publicQueue === 0 ? `` : `(+ ${serverTwo?.publicQueue})`}
-  //       </p>
-  //       <p>TPS: {serverTwo?.serverTickRate}</p>
-  //       <p>Текущая карта: {serverTwo?.currentLayer}</p>
-  //       <p>Следующая карта: {serverTwo?.nextLayer}</p>
-  //     </div>
-  //   </Popover>
-  // )
+  const speakerTwo = (
+    <Popover title={serverTwo?.serverName}>
+      <div>
+        <p>
+          Игроков: {serverTwo?.playerCount}/100{' '}
+          {serverTwo?.publicQueue === 0 ? `` : `(+ ${serverTwo?.publicQueue})`}
+        </p>
+        <p>TPS: {serverTwo?.serverTickRate}</p>
+        <p>Текущая карта: {serverTwo?.currentLayer}</p>
+        <p>Следующая карта: {serverTwo?.nextLayer}</p>
+      </div>
+    </Popover>
+  )
 
   const location = useLocation()
 
@@ -135,40 +131,40 @@ function Header() {
     <div className={styles.wrapper}>
       <ButtonToolbar>
         <ButtonGroup>
-          {/*<Whisper*/}
-          {/*  placement="bottomStart"*/}
-          {/*  trigger="hover"*/}
-          {/*  controlId="whisper1"*/}
-          {/*  speaker={speakerOne}*/}
-          {/*>*/}
-          <Button
-            onClick={() => {
-              localStorage.setItem('server', String(8000))
-              window.location.reload()
-            }}
-            appearance={server === '8000' ? 'primary' : 'default'}
-            color={'green'}
+          <Whisper
+            placement="bottomStart"
+            trigger="hover"
+            controlId="whisper1"
+            speaker={speakerOne}
           >
-            OC1
-          </Button>
-          {/*</Whisper>*/}
-          {/*<Whisper*/}
-          {/*  placement="bottomStart"*/}
-          {/*  trigger="hover"*/}
-          {/*  controlId="whisper1"*/}
-          {/*  speaker={speakerTwo}*/}
-          {/*>*/}
-          <Button
-            onClick={() => {
-              localStorage.setItem('server', String(8001))
-              window.location.reload()
-            }}
-            appearance={server === '8001' ? 'primary' : 'default'}
-            color={'green'}
+            <Button
+              onClick={() => {
+                localStorage.setItem('server', String(8000))
+                window.location.reload()
+              }}
+              appearance={server === '8000' ? 'primary' : 'default'}
+              color={'green'}
+            >
+              OC1
+            </Button>
+          </Whisper>
+          <Whisper
+            placement="bottomStart"
+            trigger="hover"
+            controlId="whisper1"
+            speaker={speakerTwo}
           >
-            OC2
-          </Button>
-          {/*</Whisper>*/}
+            <Button
+              onClick={() => {
+                localStorage.setItem('server', String(8001))
+                window.location.reload()
+              }}
+              appearance={server === '8001' ? 'primary' : 'default'}
+              color={'green'}
+            >
+              OC2
+            </Button>
+          </Whisper>
         </ButtonGroup>
       </ButtonToolbar>
       <div className={styles.search}>
@@ -205,7 +201,6 @@ function Header() {
         <Dropdown.Item panel style={{ padding: 10, width: 200 }}>
           <p>Зашел как</p>
           <strong>{getMe?.name}</strong>
-          {/*<strong>name</strong>*/}
         </Dropdown.Item>
         <Dropdown.Item divider />
         {links.map(([title, link], index) =>
